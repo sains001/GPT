8a8cb77b9a18e02889b1c56179f0ba97f2b90188
sains001 <ffsains33@gmail.com>
2026-05-16 21:52:52 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ChatGPTClone from '../ChatGPTClone.jsx'
+import ChatGPTClone from './ChatGPTClone.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
